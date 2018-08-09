1d79185fa0c5835654943cf9ef89a1ea5728daf0
tkbox9@gmail.com <wolf@WolfsMacBook.local>
2018-08-09 10:50:58 +0100

    boilerplate

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../evare.zip');
+var kuduApi = 'https://evare.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$evare';
+var password = 'skiAqjjpAcGZH3bddeYegwgdp8WM21J51k7zZd8kKhW78EfAFdD3TZa6uBGg';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('evare publish');
+  } else {
+    console.error('failed to publish evare', err);
+  }
+});
